d0d8374f31d879287e8c4d2d24e2939c094507a4
Jeadie <jack.eadie0@gmail.com>
2018-07-10 22:32:09 +1000

    add untested firebase core and database sdk to project

---
+++ convo-android/app/src/main/java/com/example/jam/pandaroo_convo/User.java
@@ -0,0 +1,21 @@
+package com.example.jam.pandaroo_convo;
+
+import java.util.List;
+import java.util.Map;
+
+public class User {
+
+    private int ID;
+    private String name;
+    private List<Integer> sessions;
+
+
+    public static User getUserFromID(int ID) {
+
+    }
+
+    public User(int ID, String name, List<Integer> focusGroupSessions) {
+
+
+    }
+}
