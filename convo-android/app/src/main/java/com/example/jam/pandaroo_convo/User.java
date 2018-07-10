691c865e99ab05cfed0e72c4638783358e4fe5f0
Jeadie <jack.eadie0@gmail.com>
2018-07-10 23:30:57 +1000

    basic User data callback

--- convo-android/app/src/main/java/com/example/jam/pandaroo_convo/User.java
+++ convo-android/app/src/main/java/com/example/jam/pandaroo_convo/User.java
@@ -1,21 +1,43 @@
 package com.example.jam.pandaroo_convo;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class User {
 
+public class User {
     private int ID;
     private String name;
     private List<Integer> sessions;
 
 
-    public static User getUserFromID(int ID) {
+    public User(int ID) {
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
 
+        // Ping firebase to grab user data. set when received data.
+        ref.child("users").child(Integer.toString(ID)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name = dataSnapshot.child("name").getValue().toString();
+                List<Object> sessions = dataSnapshot.child("focus_group").getValue(new Class<List<Object>>);
+                for (int i = 0; i < sessions.size(); i++) {
+                    if(sessions.get(i).toString().equals("True")) {
+                        sess.add(i);
+                    } else {
+                        System.out.println(String.format("%s is not True", sessions.get(i).toString()));
+                    }
+                }
+            }
+        }
     }
 
-    public User(int ID, String name, List<Integer> focusGroupSessions) {
 
-
-    }
 }
